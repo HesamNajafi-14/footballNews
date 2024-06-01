2413251df508b210b86e05d0851dc73b11bafa95
HesamNajafi <hesamnajafi39@gmail.com>
2024-06-01 16:41:30 +0330

    showing football events using by varzesh3 api in our template

--- footballNews/views.py
+++ footballNews/views.py
@@ -9,14 +9,17 @@ FOOTBALL_API_KEY = os.getenv("FOOTBALL_API_KEY")
 
 
 def get_api_response(request):
-    url = f'https://apiv3.apifootball.com/?action=get_countries&APIkey={FOOTBALL_API_KEY}'
-    
+    #url = f'https://apiv3.apifootball.com/?action=get_countries&APIkey={FOOTBALL_API_KEY}'
+    #url = f'https://apiv3.apifootball.com/?action=get_events&from=2024-06-01&to=2024-06-01&league_id=152&APIkey={FOOTBALL_API_KEY}'
+    #url = f'https://heisenbug-champions-league-live-scores-v1.p.rapidapi.com/api/championsleague/match/events?team1=Borussia%20Dortmund&team2=Real%20Madrid'
+    #url = 'https://www.chelseafc.com/en/api/fixtures/league-table?entryId=30EGwHPO9uwBCc75RQY6kg'
+    url = 'https://web-api.varzesh3.com/v1.0/livescore/today'
     response = requests.get(url)
     
     if response.status_code == 200: 
-        countrydata = response.json()
+        data = response.json()
         # Process the data as needed
-        return render(request, 'api_response.html', {'countrydata': countrydata})
+        return render(request, 'api_response.html', {'data': data})
 
 
 
